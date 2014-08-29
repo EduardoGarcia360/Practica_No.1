b44b3a87918d72bc9a0c0c081162712f7d1f76fd
EduardoGarcia360 <eduardo_garcia360@hotmail.com>
2014-08-28 18:40:15 -0600

    2

--- LoveLetter/src/Principal.java
+++ LoveLetter/src/Principal.java
@@ -1,10 +1,12 @@
-
+ 
 import java.util.*;
+
 
 public class Principal {
 	
 	public static void main(String[] args) {
 		int num = 0, tokens=0;
+		
 		System.out.println("- = Bienvenido a Love Letter = -");
 		System.out.println("(1) Jugar una partida.");
 		System.out.println("(2) Seleccionar el numero de tokens a jugar.");
@@ -25,12 +27,7 @@ public class Principal {
 			
 		switch (num){
 		case 1:
-			if(tokens==0){
-				
-					System.out.println("Antes de iniciar debe ingresar un numero de tokens.");
-					num=menu_eleccion.nextInt();
-				
-			}else{
+			
 			System.out.println("ha seleccionado jugar una partida.\nIngrese lo que a continuacion se le pide:");
 			   System.out.println("Ingrese su nombre: ");
 			   Scanner nom1 = new Scanner(System.in);
@@ -40,7 +37,20 @@ public class Principal {
 			   Scanner nom2 = new Scanner(System.in);
 			   String rival = nom2.nextLine();
 			   
-			}//fin else   
+			   System.out.println("a continuacion se decidira quien inicia");
+			   Random aleatorio = new Random();
+			   int randomNum = aleatorio.nextInt(2);
+			   randomNum=randomNum*10;
+			   if (randomNum==0){
+				   System.out.println("Inicia el jugador principal");
+			   }else{
+				   System.out.println("Inicia el jugador dos");
+			   }
+			   
+			   
+			   
+			   
+			
 	    break;
 		case 2:
 			   System.out.println("ha seleccionado el numero de tokens para jugar.\nEscriba un numero entero de 1 a 3 para el numero de tokens.");
@@ -69,7 +79,14 @@ public class Principal {
 		break;
 		case 3:
 			   System.out.println("A continuacion se muestran las reglas del juego:");
-			   
+			   System.out.println("(1) Guard:\nPermite adivinar la mano del rival (cartas del 2-8) si adivina,\nel jugador rival sale del juego. (5 en el mazo)");
+			   System.out.println("(2) Priest:\nPermite ver la mano del jugador rival. (2 en el mazo)");
+			   System.out.println("(3) Baron:\nCompara manos con el rival, el jugador con la carta mas baja sale del juego.\nEn caso de empate no sucede nada. (2 en el mazo)");
+			   System.out.println("(4) Handmaid:\nEl jugador que utiliza esta carta es inmune a los ataques\nde los demas por un (1) turno. (2 en el mazo)");
+			   System.out.println("(5) Prince:\nPermite “botar” la mano del jugador o la del rival.\nY da una carta nueva del deck. (2 en el mazo)");
+			   System.out.println("(6) King:\nIntercambia manos entre jugadores.(1 en el mazo)");
+			   System.out.println("(7) Countess:\nSi el jugador tiene un Prince o King en su mano\ny la Countess debe jugar la Countess. (1 en el mazo)");
+			   System.out.println("(8) Princess:\nSi el jugador juega esta carta pierde el juego. (1 en el mazo)");
 		break;
 		case 4:
 			   System.exit(1);
